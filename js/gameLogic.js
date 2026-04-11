3e2aac36c8625fdf83f300670c8dfb12bc807374
Oleksandr Popov <kripopovaa@gmail.com>
2026-04-11 12:17:39 +0300

    функція з словами + нові слова + Input в целах

--- js/gameLogic.js
+++ js/gameLogic.js
@@ -1,2 +1,14 @@
 let currentRow = 1;
+let status = 'playing';
+
+function getRandomWordWithLetters(words) {
+  const randomIndex = Math.floor(Math.random() * words.length);
+  const randomWord = words[randomIndex];
+  
+  return [randomWord, randomWord.split('')];
+}
+
+const word = getRandomWordWithLetters(WORDS);
+console.log("Слово: " + word[0]);
+
 
