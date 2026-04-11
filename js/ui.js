6a1e198e60da1283025275ab9ac3b3839d7b6c68
Роман <romiknishchuk2099@gmail.com>
2026-04-11 12:42:24 +0300

    feat: added ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,12 @@
+// function getGameState(){
+//   board = [ ['A','B','','',''],
+//   ['C','','','',''] ];
+//   currentRow: 0,
+//   console.error(null);
+// };
 
+// function renderBoard(){
+//     getGameState();
+//     const container = document.querySelector(".GameBoard");
+//     container.innerHTML = "";
+// }
